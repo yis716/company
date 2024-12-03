d865ccfccefb9488cf140c5728e5344c1f428d60
yis <https://github.com/yis716/company.git>
2024-12-04 00:57:32 +0900

    sub4_1 차트 js 시작

---
+++ sub4/js/main_chart.js
@@ -0,0 +1,90 @@
+const cht1 = document.getElementById('myChart1');
+const cht2 = document.getElementById('myChart2');
+const cht3 = document.getElementById('myChart3');
+const cht4 = document.getElementById('myChart4');
+
+const chart = new Chart(cht1, {
+    type: 'bar',//차트 종류
+    data: {
+        labels: ['2021', '2022', '2023'],
+        datasets: [{
+            label: '매출액',
+            data: [95172, 110319, 116125],
+            backgroundColor: [
+                'rgba(245, 131, 27, 0.2)', // labels[0]
+                'rgba(153, 204, 0, 0.2)', // labels[1]
+                'rgba(3, 122, 254, 0.2)'  // labels[2]
+            ],
+            borderColor: [
+                'rgb(245, 131, 27)', // labels[0]
+                'rgb(153, 204, 0)', // labels[1]
+                'rgb(3, 122, 254)'  // labels[2]
+            ],
+            borderWidth: [
+                5
+            ],
+            borderRadius: [
+                10
+            ],
+            maxBarThickness: [
+                150 // 최대 bar의 두께 설정
+            ]
+        },
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                grid: { // 축에 대한 격자선
+                    display: false, // grid 활성화 (기본값 true)
+                },
+                beginAtZero: true //0부터 시작
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            // labels: {
+            //     font: {
+            //         size: 26
+            //     },
+            // }    
+        }
+        // legend: {
+        //     labels: {
+        //         // 이 더 특정한 폰트 속성은 전역 속성을 덮어씁니다
+        //         fontColor: 'red',
+        //         fontFamily: "'Noto Sans KR'",
+        //         fontSize: 40
+        //     }
+        // }
+    },
+});
+
+
+// const config = {
+//     options: {
+//         scales: {
+//             y: {
+//                 grid: { // 축에 대한 격자선
+//                     display: false, // grid 활성화 (기본값 true)
+//                 },
+//                 beginAtZero: true //0부터 시작
+//             }
+//         },
+//         plugins: {
+//             legend: {
+//                 display: false
+//             }
+//         }
+//         // legend: {
+//         //     labels: {
+//         //         // 이 더 특정한 폰트 속성은 전역 속성을 덮어씁니다
+//         //         fontColor: 'red',
+//         //         fontFamily: "'Noto Sans KR'",
+//         //         fontSize: 40
+//         //     }
+//         // }
+//     },
+// };
